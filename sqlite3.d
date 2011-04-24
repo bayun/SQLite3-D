0b9a7be05daca9a4ea77bd5ea8498f1aa5aa6afc
Bayun <bayun@progressors.org>
2011-04-24 21:47:26 +0300

    Changed license to Boost 1.0

--- sqlite3.d
+++ sqlite3.d
@@ -1,29 +1,11 @@
-/*
-* Copyright 2010 Alexey Khmara. All rights reserved.
-* 
-* Redistribution and use in source and binary forms, with or without modification, are
-* permitted provided that the following conditions are met:
-* 
-*    1. Redistributions of source code must retain the above copyright notice, this list of
-*       conditions and the following disclaimer.
-* 
-*    2. Redistributions in binary form must reproduce the above copyright notice, this list
-*       of conditions and the following disclaimer in the documentation and/or other materials
-*       provided with the distribution.
-* 
-* THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
-* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-* ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-* 
-* The views and conclusions contained in the software and documentation are those of the
-* authors and should not be interpreted as representing official policies, either expressed
-* or implied, of Alexey Khmara.
+/**
+* Copyright: Copyright Alexey Khmara 2010 - 2011
+* License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>
+* Authors:   Alexey Khmara, alex.khmara@gmail.com
+*
+* Copyright Alexey Khmara 2010 - 2011. Distributed under Boost License 1.0
+* (see accompanying file LICENSE_1_0.txt) or 
+* at http://www.boost.org/LICENSE_1_0.txt
 */
 
 module sqlite3;
